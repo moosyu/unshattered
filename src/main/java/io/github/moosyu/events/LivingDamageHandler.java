182509f1be07af2b49a8b10b3a611f4b84b042d1
moosyu <70246651+moosyu@users.noreply.github.com>
2026-05-19 08:04:46 +1200

    on feet, by himself!!

--- src/main/java/io/github/moosyu/events/LivingDamageHandler.java
+++ src/main/java/io/github/moosyu/events/LivingDamageHandler.java
@@ -2,6 +2,8 @@ package io.github.moosyu.events;
 
 import io.github.moosyu.registers.AttributesRegistry;
 import net.minecraft.tags.DamageTypeTags;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -14,19 +16,21 @@ public class LivingDamageHandler {
     public static class EventHandler {
         @SubscribeEvent
         public static void onLivingDamage(LivingDamageEvent.Pre event) {
-            // todo: this currently blows up my computers and doesnt work so maybe i should fix that
             if (event.getEntity() instanceof Player player) {
-                // disabling the default damage as there's it cant be cancelled
                 event.setNewDamage(0.0f);
+                Entity attacker = event.getSource().getEntity();
+                if (attacker instanceof LivingEntity entity) {
+                    double mobDamage = entity.getAttribute(AttributesRegistry.DAMAGE).getBaseValue();
+                    float currentHealth = player.getHealth();
 
-                double mobDamage = event.getEntity().getAttribute(AttributesRegistry.DAMAGE).getBaseValue();
-                double playerHealth = player.getAttribute(AttributesRegistry.HEALTH).getBaseValue();
-
-                if (playerHealth - mobDamage > 0.0d) {
-                    player.hurt(event.getSource(), 0.0f);
-                    player.getAttribute(AttributesRegistry.HEALTH).setBaseValue(playerHealth - mobDamage);
-                } else {
-                    player.kill();
+                    if (currentHealth - mobDamage > 0.0f) {
+                        player.setHealth((float)(currentHealth - mobDamage));
+                    } else {
+                        // this doesnt work, i assume due to setting damage to 0.0f earlier.
+                        // probably fix by doing the same thing with healing manager
+                        System.out.println("death triggered");
+                        player.kill();
+                    }
                 }
             } else if (event.getSource().is(DamageTypeTags.IS_FIRE)) {
                 // seems to be required to allow players to damage enemies on fire
